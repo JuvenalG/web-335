edd8ad786ba4a2df5a07cd27740ed1aab032d430
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-12-07 00:49:26 -0800

    c

---
+++ week-8/gonzalez_hello_world.py
@@ -0,0 +1,9 @@
+
+""" ============================================
+; Title:  Assignment 8.2
+; Author: Juvenal Gonzalez
+; Date:   December 7, 2020
+; Description: output hello world
+;=========================================== """
+
+print("Hello World")
